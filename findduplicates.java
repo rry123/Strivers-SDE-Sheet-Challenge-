b0ad6dd1f605f0a259f95f01272cb3c9744b0176
rry123 <94428747+rry123@users.noreply.github.com>
2023-06-05 21:17:25 +0530

    Add files via upload

---
+++ findduplicates.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.* ;
+
+import java.util.ArrayList;
+
+public class Solution{
+    public static int findDuplicate(ArrayList<Integer> arr, int n){
+        // Write your code here.
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i<n; i++){
+            if(!set.contains(arr.get(i))){
+                set.add(arr.get(i));
+            }
+            else{
+                return arr.get(i);
+            }
+        }
+        return -1;
+    }
+}
