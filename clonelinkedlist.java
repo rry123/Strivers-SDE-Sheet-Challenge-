611efb4d6d16e89c8f840b0536434884101b1e38
rry123 <94428747+rry123@users.noreply.github.com>
2023-06-20 11:52:00 +0530

    Add files via upload

---
+++ clonelinkedlist.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.* ;
+
+/*************************************************************
+
+    Following is the LinkedListNode class structure
+
+    class LinkedListNode<T> {
+	    T data;
+	    LinkedListNode<T> next;
+	    LinkedListNode<T> random;
+
+	    public LinkedListNode(T data) {
+		    this.data = data;
+	    }
+    }
+
+*************************************************************/
+
+public class Solution {
+	public static LinkedListNode<Integer> cloneRandomList(LinkedListNode<Integer> head) {
+		// Write your code here.
+		 LinkedListNode<Integer> iter = head;
+        LinkedListNode<Integer> front = head;
+        
+        while(iter != null){
+            front =iter.next;
+            LinkedListNode<Integer> copy = new LinkedListNode<Integer>(iter.data);
+            iter.next =copy;
+            copy.next = front;
+            iter=front;
+        }
+        
+        iter = head;
+        while(iter!=null){
+            if(iter.random!=null){
+                iter.next.random = iter.random.next;
+            }
+            iter = iter.next.next;
+        }
+        
+        iter = head;
+        LinkedListNode<Integer> fakehead = new LinkedListNode<Integer>(0);
+        LinkedListNode<Integer> copy = fakehead;
+        while(iter!=null){
+            front = iter.next.next;
+            copy.next = iter.next;
+            copy = copy.next;
+            iter.next = front;
+            iter =front;
+        }
+        return fakehead.next;
+	}
+}
